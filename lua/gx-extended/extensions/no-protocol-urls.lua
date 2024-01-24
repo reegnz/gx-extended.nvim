3827299ddd8100c8b341cb1567dd9ef049306ba3
Zoltán Reegn <zoltan.reegn@gmail.com>
2024-01-24 13:54:53 +0100

    Add match around to built-in extensions
    Match should respect not just the line but the cursor location as well,
    since the line might contain targets at multiple places that are matched
    by different extensions.

--- lua/gx-extended/extensions/no-protocol-urls.lua
+++ lua/gx-extended/extensions/no-protocol-urls.lua
@@ -6,22 +6,24 @@ function M.setup(config)
     name = "no-protocol-urls",
     match_to_url = function(line_string)
       local patterns_with_http_s = "(https?://[a-zA-Z0-9_/%-.~@#+=?&%%A-Fa-f]+)"
-      local patterns_without_http_s = "([a-zA-Z0-9_/%-.~@#+%%A-Fa-f]+%.[a-zA-Z0-9_/%-.~@#+%%A-Fa-f%=?&]+)"
+      local patterns_without_http_s = "([a-zA-Z0-9_/%-.~@#+?&%%A-Fa-f]+)"
 
-      local url = string.match(line_string, patterns_with_http_s)
-
-      if not url then
-        url = string.match(line_string, patterns_without_http_s)
-        -- Validate that it starts with a valid-ish domain
-        if not string.match(url, "%S+%.%S+%.[%w%.]+/?.*") then
-          return nil
-        end
-
-        if url then
-          url = "https://" .. url
-        end
+      local col = vim.fn.col "."
+      local match_start, match_end, url = string.find(line_string, patterns_with_http_s)
+      if url and match_start <= col and match_end >= col then
+        return url
       end
-
+      match_start, match_end, url = string.find(line_string, patterns_without_http_s)
+      if not url or match_start > col or match_end < col then
+        return nil
+      end
+      -- Validate that it starts with a valid-ish domain
+      if not string.match(url, "%S+%.%S+%.[%w%.]+/?.*") then
+        return nil
+      end
+      if url then
+        url = "https://" .. url
+      end
       return url
     end,
   }
